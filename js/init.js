23b7d19f84555d6522dc20e6b586a8210dcbfe05
Tony Worm <verdverm@gmail.com>
2015-09-28 19:28:13 -0400

    added more data, updated others, added images

--- js/init.js
+++ js/init.js
@@ -2,6 +2,10 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+	$('.slider').slider({
+		height: 300,
+		interval: 4000,
+	});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
